ccec08f88083ccaaa36c30c0e381060b902017b0
Colin McFadden <cmcfadden@gmail.com>
2021-04-06 14:09:10 -0500

    colin's proof of concept work

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,8 @@
 <template
     ><div>
         <div class="question" id="question" @mouseup="getSelectedText">
+            <p v-html="mytext">
+                </p>
             <p>
                 Schaefer’s job, with a Dublin-based solar panel startup, was to
                 write an e-book
@@ -34,14 +36,60 @@ export default {
         return {
             counter: 0,
             response: '',
+            savedText: "Schaefer’s job, with a Dublin-based solar panel startup, was to write an e-book",
+            startOffset: 0,
+            endOffset: 0
         };
+    },
+    computed: {
+        mytext: function() {
+            var outputString = "";
+
+            var student1 = {
+                startOffset: 10,
+                endOffset: 20
+            }
+            var student2 = {
+                startOffset: 12,
+                endOffset: 23
+            }
+            var student3 = {
+                startOffset: 2,
+                endOffset: 15
+            }
+            var responses = [student1, student2, student3];
+            var histogram = [];
+            for(var i=0; i<responses.length; i++) {
+                var currentStudent = responses[i];
+                for(var k = currentStudent.startOffset; k<currentStudent.endOffset; k++) {
+                    if(histogram[k]) {
+                        histogram[k]++;
+                    }
+                    else {
+                        histogram[k] = 1;
+                    }
+                }
+            }
+            console.log(histogram);
+
+            for(i=0; i<this.savedText.length; i++) { 
+                if(i>= this.startOffset && i < this.endOffset) {
+                    var colorString = "rgba(100,100,100, " + 1 * histogram[i] / 10 + ")";
+                    outputString = outputString + "<span style='color: " + colorString + "'>" + this.savedText[i] + "</span>";
+                }
+                else {
+                    outputString = outputString + this.savedText[i];
+                }
+            }
+
+            return outputString;
+        }
     },
     methods: {
         getSelectedText(e) {
             this.counter++;
             const mySelection = window.getSelection(e);
-
-            // console.log(mySelection);
+            console.log(mySelection);
 
             // console.log('anchor: ' + mySelection.anchorNode); // returns node where selection (mousedown) started
             // console.log('focus: ' + mySelection.focusNode); // returns node where selection (mousedown) ends
@@ -56,31 +104,35 @@ export default {
             // only works inside of a single node, not when selection spans nodes. throws an error "InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable"
             // it does nest mark tags, but again, slection breaks if second selection is not fully contained
             const range = mySelection.getRangeAt(0);
+            console.log(range);
+            this.startOffset = range.startOffset;
+            this.endOffset = range.endOffset;
+
             // const newParent = document.createElement('mark');
             // const newRange = range.surroundContents(newParent);
-            // console.log('new range: ' + range);
+            console.log('new range: ' + range);
             // this.response = newRange;
             // return newRange;
 
             // similar to surroundContents, but handles crossing nodes and partial nodes better. Still only works as expected within a single node.
-            const selectionContents = range.extractContents();
+            // const selectionContents = range.extractContents();
             // console.log(selectionContents);
 
-            // range = document.createRange();
-            const newNode = document.createElement('mark');
-            newNode.appendChild(selectionContents);
-            range.insertNode(newNode);
+            // // range = document.createRange();
+            // const newNode = document.createElement('mark');
+            // newNode.appendChild(selectionContents);
+            // range.insertNode(newNode);
 
-            // get new question with highlights
-            const fullResult = document
-                .getElementById('question')
-                .cloneNode('deep');
-            console.log(fullResult);
-            // document.getElementById('response').append(fullResult);
+            // // get new question with highlights
+            // const fullResult = document
+            //     .getElementById('question')
+            //     .cloneNode('deep');
+            // console.log(fullResult);
+            // // document.getElementById('response').append(fullResult);
 
-            document
-                .getElementById('response')
-                .insertAdjacentElement('afterbegin', fullResult);
+            // document
+            //     .getElementById('response')
+            //     .insertAdjacentElement('afterbegin', fullResult);
             // this.response = fullResult;
             // return fullResult;
         },
